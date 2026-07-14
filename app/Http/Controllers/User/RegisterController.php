91be68ca00db27f54b10719567c3511a3c26e734
satomi <matsushima@genten-group.co.jp>
2026-07-14 23:06:51 +0900

    登録コントローラーの修正

--- app/Http/Controllers/User/RegisterController.php
+++ app/Http/Controllers/User/RegisterController.php
@@ -1,1 +1,23 @@
-git add .
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRegisterRequest; 
+use App\Models\User; 
+
+class RegisterController extends Controller
+{
+    public function showRegisterForm()
+    {
+        return view('user.register'); 
+    }
+
+    public function register(UserRegisterRequest $request)
+    {
+        // 💡 DBへの保存処理をモデルに任せる！
+        $user = User::register($request->all());
+
+        return back()->with('success', 'ユーザー登録が完了しました！');
+    }
+}
